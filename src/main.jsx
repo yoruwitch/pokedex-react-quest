37ad3a4d0f1ca20e6bf074f926c0bd0c19bec05c
yoruwitch <evelyn.fernandes4@gmail.com>
2025-05-12 22:05:01 -0300

    feat: update project structure and dependencies for Pokédex app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+        </Routes>
+    </BrowserRouter>
+);
